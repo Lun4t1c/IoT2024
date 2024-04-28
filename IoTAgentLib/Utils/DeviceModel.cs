63b956f52e757f7636bf8ec38431a6813266314f
Lun4t1c <artur.lunat1c.grabowski@gmail.com>
2024-04-28 12:13:18 +0200

    Basic DeviceModel

--- IoTAgentLib/Utils/DeviceModel.cs
+++ IoTAgentLib/Utils/DeviceModel.cs
@@ -8,9 +8,29 @@ namespace IoTAgentLib.Utils
 {
     public class DeviceModel
     {
+        #region Properties
+        public bool ProductionStatus { get; set; }
+        public Guid WorkorderId { get; set; }
+        public short ProductionRate { get; set; }
+        public uint GoodCount { get; set; }
+        public uint BadCount { get; set; }
+        public short Temperature { get; set; }
+        public byte DeviceErrors { get; set; } = 0000;
+        #endregion
+
         public DeviceModel()
         {
             
+        }
+
+        public void PerformEmergencyStop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetErrorStatus()
+        {
+            throw new NotImplementedException();
         }
     }
 }
